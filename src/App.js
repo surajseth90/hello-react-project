1a32e0078d5b23261af298fc34dcaa32b3582e89
surajseth90 <surajseth90@gmail.com>
2021-06-30 20:55:29 +0530

    Added Brigelabz page with complete text

--- src/App.js
+++ src/App.js
@@ -7,42 +7,59 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-     user:'',
-     nameError:''
+      user: '',
+      nameError: ''
     }
   }
 
-  onClick=(event) =>{
-    window.open(this.url,"_blank");
+  onClick = (event) => {
+    window.open(this.url, "_blank");
   }
 
-  onNameChange = (event)=>{
+  onNameChange = (event) => {
     const nameRegExp = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
-    this.setState({user:event.target.value})
-    if(nameRegExp.test(event.target.value)){
-      this.setState({nameError:''})
-    }else{
-      this.setState({nameError:'Name is in Incorrect Format'})
+    if (nameRegExp.test(event.target.value)) {
+      this.setState({ nameError: '' })
+      this.setState({ user: event.target.value })
+    } else {
+      this.setState({ nameError: 'Name is in Incorrect Format' })
     }
   }
 
   render() {
     return (
-      <>
-      <div>
-        <h1>
-        Hello {this.state.user} from bridgelabz
-        </h1>
-        <img src={logo} onClick={this.onClick} alt="bridgelabz"/>
-      </div>
+      <div className="app-main">
+        <div className="main">
+          <div>
+            <h1>
+              Hello {this.state.user} from bridgelabz
+            </h1>
+            <img src={logo} onClick={this.onClick} alt="bridgelabz" />
+          </div>
 
-      <div>
-        <input onChange={this.onNameChange}/>
-        <span className="error-output">{this.state.nameError}</span>
+          <div>
+            <input onChange={this.onNameChange} />
+            <span className="error-output">{this.state.nameError}</span>
+          </div>
+
+          <div>
+            A Bridgelabz we're a community of
+            <ul>
+              <li>
+                technologist
+              </li>
+              <li>thinkers</li>
+              <li>builders</li>
+            </ul>
+            Working together to keep the Tech Employebility of Engineers alive......
+            <br />
+            <br />
+            To know about us, visit <a href="https://www.bridgelabz.com/" ref="BridgeLabz" target="_blank">BridgeLabz</a> to learn even more about our mission i.e. <b>Employability to all.</b>
+          </div>
+        </div>
       </div>
-      </>
-        );
-        
+    );
+
   }
 }
-        export default App;
+export default App;
